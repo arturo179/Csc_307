bf3448f738a3096af88fcfe4f760387e7dbfec43
Arturo <artio9000@gmail.com>
2024-10-02 08:05:42 -0700

    Part 6 complete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -71,6 +71,16 @@ const users = {
       res.send(result);
     }
   });
+  const addUser = (user) =>{
+    users["users_list"].push(user);
+    return user;
+
+  };
+  app.post("/users", (req, res)=> {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
 
 app.listen(port, () => {
   console.log(
