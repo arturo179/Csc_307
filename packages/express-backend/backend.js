6ed444a103539069bb54fde97881231614616031
Arturo <artio9000@gmail.com>
2024-10-02 20:45:25 -0700

    Final Part of Rest

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,9 +6,9 @@ const port = 8000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 const users = {
     users_list: [
       {
@@ -42,25 +42,28 @@ const users = {
 //   app.get("/users", (req, res) => {
 //     res.send(users);
 //   });
-  const findUserByName = (name) => {
-    return users["users_list"].filter(
-      (user) => user["name"] === name
+const findUserByNameAndJob = (name, job) => {
+    return users.users_list.filter((user) => 
+        user.name.toLowerCase() === name.toLowerCase() && user.job.toLowerCase() === job.toLowerCase()
     );
-  };
+};
   
   app.get("/users", (req, res) => {
     const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
+    const job = req.query.job;
+    if (name != undefined && job != undefined) {
+      let result = findUserByNameAndJob(name,job);
+      result= {users_list: result};
       res.send(result);
     } else {
       res.send(users);
     }
   });
 
-  const findUserById = (id) =>
+  const findUserById = (id) =>{
     users["users_list"].find((user) => user["id"] === id);
+    return id;
+  };  
   
   app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; //or req.params.id
@@ -71,11 +74,37 @@ const users = {
       res.send(result);
     }
   });
+  
+  
+    
+    
+    
   const addUser = (user) =>{
     users["users_list"].push(user);
     return user;
 
   };
+  const deleteUser = (id) => {
+    const userIndex = users["users_list"].findIndex((user) => user["id"] === id);
+    
+    users["users_list"].splice(userIndex, 1);
+    return id; 
+};
+
+app.delete("/users/:id", (req, res) => {
+    const userToDelete = req.params.id; // Get the ID from the URL params
+    let result = deleteUser(userToDelete);
+    
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(); 
+    }
+}); 
+        
+        
+        
+    
   app.post("/users", (req, res)=> {
     const userToAdd = req.body;
     addUser(userToAdd);
