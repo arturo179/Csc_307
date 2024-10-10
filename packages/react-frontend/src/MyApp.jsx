dbb79fccb7e0da281f4b81bba1fd37dea8ad3ab4
Arturo <artio9000@gmail.com>
2024-10-09 18:29:00 -0700

    Final Part Done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,19 +1,81 @@
 // src/MyApp.jsx
-import React,{ useState } from "react";
+import React,{ useState,useEffect } from 'react';
 import Table from "./Table";
 import Form from"./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+  
   function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
+    const userToDelete = characters[index];
+    if(!userToDelete) return;
+    fetch(`http://localhost:8000/users/${userToDelete.id}`,{
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response)=> {
+      if(response.ok){
+
+
+        const updatedCharacters = characters.filter((_,i) => i !== index);
+        setCharacters(updatedCharacters);
+        
+      }else{
+        throw new Error(404)
+      }
+    })
+    .catch((error) => {
+      console.log(error);
     });
-    setCharacters(updated);
+    // const updated = characters.filter((character, i) => {
+    //   return i !== index;
+    // });
+    // setCharacters(updated);
   }
+    
+  
+    
+  
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+    .then((response) =>{
+      if(response.status ===201){
+        return response.json();
+      } else{
+        throw new Error('Failed to create')
+      }
+    })
+
+    .then((newUser) => setCharacters([...characters, newUser]))
+    .catch((error) => {
+      console.log(error);
+    });
   }
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+}
+useEffect(() => {
+  fetchUsers()
+	  .then((res) => res.json())
+	  .then((json) => setCharacters(json["users_list"]))
+	  .catch((error) => { console.log(error); });
+  
+}, [] );
+function postUser(person) {
+  const promise = fetch("http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+
+  });
+
+  return promise;
+}
   return (
     <div className="container">
       <Table 
